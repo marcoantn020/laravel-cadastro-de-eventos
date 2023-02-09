fcc3602e213e0239a14357e658b540487380f1c4
marcoantn020 <marcoantn020@gmail.com>
2023-02-08 23:04:26 -0300

    buscando eventos

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -6,14 +6,18 @@
 
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
-    <form action="" method="post">
+    <form action="/" method="get">
         <input type="text" name="search" id="search" class="form-control" placeholder="Procurar">
     </form>
 </div>
 
 <div id="events-container" class="col-md-12">
-    <h2>Proximos eventos</h2>
-    <p class="subtitle">Veja os eventos dos proximos dias</p>
+    @if($search)
+        <h2>Buscando por: {{ $search }}</h2>
+    @else
+        <h2>Proximos eventos</h2>
+        <p class="subtitle">Veja os eventos dos proximos dias</p>
+    @endif
     <div id="cards-container" class="row">
         @foreach($events as $event)
             <div class="card col-md-3">
@@ -26,7 +30,11 @@
                 </div>
             </div>
         @endforeach
-        @if(count($events) === 0)
+        @if(count($events) === 0 && $search)
+            <p>Não há foi possivel encontrar nenhum evento com {{ $search }}!
+                <a href="/">Ver todos</a>
+            </p>
+        @elseif(count($events) === 0)
             <p>Não há eventos disponiveis</p>
         @endif
     </div>
