cb42d02305d968900742ab669dcde7db23964c2f
marcoantn020 <marcoantn020@gmail.com>
2023-02-11 14:00:16 -0300

    deletando dados

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -67,4 +67,10 @@ class EventController extends Controller
 
         return view('events.dashboard', ['events' => $events]);
     }
+
+    public function destroy($id)
+    {
+        Event::findOrFail($id)->delete();
+        return redirect('/dashboard')->with('msg', 'evento excluido com sucesso.');
+    }
 }
--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 Route::post('/events', [EventController::class, 'store']);
 Route::get('/events/{id}', [EventController::class, 'show']);
+Route::delete('/events/{id}', [EventController::class, 'destroy']);
 
 Route::get('/contact', function() {return view('contact');});
 
